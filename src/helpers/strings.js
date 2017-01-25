c5153a44f38ca1f22a712ac0b5dbc65b6b082351
spences10 <spences10apps@gmail.com>
2017-01-25 19:32:18 +0000

    Initial commit

---
+++ src/helpers/strings.js
@@ -0,0 +1,29 @@
+module.exports = {
+    queryString: [
+        '100daysofcode',
+        '301daysofcode',
+        'freecodecamp',
+        'github',
+        'vscode',
+        'visual studio code',
+        'atomio',
+        'atom.io',
+        'nodejs',
+        'node.js',
+        'npmjs',
+        'vuejs',
+        'vue.js',
+        'inferno_js',
+        'infernojs',
+        'inferno.js',
+        'jekyll',
+        'laravel',
+        'laravelphp',
+        'conditioner.js',
+        'conditionerjs',
+        'svelte.js',
+        'sveltejs',
+        'docker'
+    ],
+    resultType: ['mixed', 'recent', 'popular']
+};
