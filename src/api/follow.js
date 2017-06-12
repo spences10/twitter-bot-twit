30c412cbb4de6847d1d1274c72ec5c021662c898
spences10 <spences10apps@gmail.com>
2017-06-12 22:02:09 +0100

    get keywords 🔑

--- src/api/follow.js
+++ src/api/follow.js
@@ -1,6 +1,7 @@
 const Twit = require('twit')
 const unique = require('unique-random-array')
 const config = require('../config')
+const fetch = require('node-fetch')
 
 const param = config.twitterConfig
 const queryString = unique(param.queryString.split(','))
@@ -10,7 +11,7 @@ const bot = new Twit(config.twitterKeys)
 const follow = () => {
 
   const query = queryString()
-
+  console.log('FOLLOW QUERY: ', query)
   bot.get('search/tweets', {
     q: query,
     result_type: param.resultType,
@@ -28,15 +29,38 @@ const follow = () => {
 
         const d = new Date()
         d.getMonth(d.getMonth() - 12)
-        console.log(`Month is ${d.toISOString()}`)
+        // console.log(`Month is ${d.toISOString()}`)
+        
         bot.get('search/tweets', {
           q: `from:@${screen_name} since:${d.toISOString()}`,
-          count: 200
+          count: 10
         }, (err, data, res) => {
           if (err) {
             console.log('ERRORDERP: ', err)
           } else {
             data.statuses.forEach(s => {
+              // get avoid keywords
+              const avoidKeywordsEndpoint = 'https://gist.githubusercontent.com/spences10/d21c8b869f3e9ccdcb12e61cd8d2ed03/raw/d428b883f838e91bd2f83e6a81b99b09a16b7b33/avoid-profile-keywords.json'
+
+              // get follow keywords
+              const followKeywordsEndpoint = 'https://gist.githubusercontent.com/spences10/d21c8b869f3e9ccdcb12e61cd8d2ed03/raw/d428b883f838e91bd2f83e6a81b99b09a16b7b33/interest-profile-keywords.json'
+
+              const getWords = async (url) => {
+                const response = await fetch(url)
+                return await response.json()
+              }
+              
+              getWords(avoidKeywordsEndpoint)
+                .then(word => {
+                  const arrAvoid = Object.values(word)
+                  console.log(typeof arrAvoid)
+                })
+
+              getWords(followKeywordsEndpoint)
+                .then(word => {
+                  console.log(word)
+                })
+              
               console.log(s.text)
               console.log(s.user.screen_name)
               console.log('\n')
