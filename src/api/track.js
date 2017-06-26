174a50c3e3c63fea04e95a1dfe08407cf86b263d
spences10 <spences10apps@gmail.com>
2017-06-26 05:59:59 +0100

    amend track.js logic

--- src/api/track.js
+++ src/api/track.js
@@ -6,11 +6,10 @@ const bot = new Twit(config.twitterKeys)
 const track = event => {
   // console.log(JSON.stringify(event.lang))
   // event.source.screen_name
-  if (event.lang != config.twitterConfig.language) {
-    console.log('====================')
-    console.log(event.text)
-    console.log('====================')
-  }
+  if (event.lang != config.twitterConfig.language) return
+  console.log('====================')
+  console.log(event.text)
+  console.log('====================')
 }
 
 module.exports = track
