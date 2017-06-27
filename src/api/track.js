711710ec28f50b3b811be2fcf543586761336889
spences10 <spences10apps@gmail.com>
2017-06-27 17:47:59 +0100

    RT in trackins search terms

--- src/api/track.js
+++ src/api/track.js
@@ -3,13 +3,23 @@ const config = require('../config')
 
 const bot = new Twit(config.twitterKeys)
 
-const track = event => {
+const track = (event) => {
   // console.log(JSON.stringify(event.lang))
+  // console.log(JSON.stringify(event))
   // event.source.screen_name
   if (event.lang != config.twitterConfig.language) return
   console.log('====================')
   console.log(event.text)
   console.log('====================')
+  // don't retweet replies
+  if (!event.in_reply_to_status_id) {
+    bot.post('statuses/retweet/:id', { id: event.id_str }, (err, res) => {
+      if (err) {
+        console.log('RETWEET ERRORDERP: ', err.message)
+      }
+      console.log('RT SUCCESS: ', event.text)
+    })
+  }
 }
 
 module.exports = track
