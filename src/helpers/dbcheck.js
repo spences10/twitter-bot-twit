569b68dc5f070d390a2b02bb381275ad9331aced
spences10 <spences10apps@gmail.com>
2017-07-17 17:52:43 +0100

    autation and query added

--- src/helpers/dbcheck.js
+++ src/helpers/dbcheck.js
@@ -1,29 +1,62 @@
-const Lokka = require('lokka').Lokka
-const Transport = require('lokka-transport-http').Transport
+// here we want to check that the tweet text does not match
+// anything that has already been retweeted
+const { request, GraphQLClient } = require('graphql-request')
 
-const headers = {
-  Authorization: 'Bearer YOUR_AUTH_TOKEN'
-}
+const endpoint = 'https://api.graph.cool/simple/v1/cj54a2vg7uvcm0196cqwzu8nw'
 
-const client = new Lokka({
-  transport: new Transport('https://api.graph.cool/simple/v1/cj54a2vg7uvcm0196cqwzu8nw', { headers })
-})
+const queryAllTweets = `{
+  allTweets {
+    tweetText
+  }
+}`
 
-function getItems() {
-  return client
-    .query(
-      `
-      query	{
-      allTweets {
-        tweetText
-        
-      }
-    }
-  `
-    )
-    .then((result) => {
-      console.log(result.allTweets)
-    })
-}
+const mutation = `mutation {
+  createTweet(tweetText: "Hi", userName: "rando") {
+    id
+  }
+}`
 
-getItems()
+const querySpecificTweet = `{
+  allTweets(filter: {tweetText: "hi dave"}) {
+    id
+  }
+}`
+
+request(endpoint, queryAllTweets).then((data) => console.log(data))
+
+request(endpoint, mutation).then((data) => console.log(data))
+
+request(endpoint, querySpecificTweet).then((data) => console.log(data))
+
+// // items from twitter
+// const Twit = require('twit')
+// const config = require('../config')
+// const bot = new Twit(config.twitterKeys)
+
+// bot.get(
+//   'followers/ids',
+//   {
+//     screen_name: config.twitterConfig.username,
+//     count: 200
+//   },
+//   function getData(err, data, response) {
+//     data.users.forEach((user) => {
+//       console.log('loop item: ', user.screen_name)
+//     })
+
+//     // for (let i = 0; i < response.users.length; i++) {
+//     //   console.log(response.users[i])
+//     // }
+
+//     if (data['next_cursor'] > 0)
+//       bot.get(
+//         'followers/ids',
+//         {
+//           screen_name: config.twitterConfig.username,
+//           count: 200,
+//           next_cursor: data['next_cursor']
+//         },
+//         getData
+//       )
+//   }
+// )
