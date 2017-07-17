85a3718cc6334cdcbf18f7c41cf5413278e98551
spences10 <spences10apps@gmail.com>
2017-07-17 22:05:33 +0100

    Add variables to mutation

--- src/helpers/dbaddtweet.js
+++ src/helpers/dbaddtweet.js
@@ -3,13 +3,17 @@ const { request } = require('graphql-request')
 const endpoint = 'https://api.graph.cool/simple/v1/cj54a2vg7uvcm0196cqwzu8nw'
 
 const addTweet = (event) => {
-  const mutation = `mutation {
-    createTweet(tweetText: "${event.text}", userName: "${event.user.screen_name}") {
+  const mutation = `mutation ($tweetText: String!, $userName: String!) {
+    createTweet(tweetText: $tweetText, userName: $userName) {
       id
     }
   }`
 
-  request(endpoint, mutation)
+  const variables = {
+    tweetText: event.text,
+    userName: event.user.screen_name
+  }
+  request(endpoint, mutation, variables)
     .then((data) => console.log(data))
     .catch((err) => console.log('Error: ', err, 'Tweet Text: ', event.text, 'Mutation: ', mutation))
 }
