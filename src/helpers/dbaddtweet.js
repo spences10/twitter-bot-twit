8a3611eb2aa770617f8e66baa39a01d9d2c2ec35
spences10 <spences10apps@gmail.com>
2017-07-18 06:33:21 +0100

    comments

--- src/helpers/dbaddtweet.js
+++ src/helpers/dbaddtweet.js
@@ -2,6 +2,8 @@ const { request } = require('graphql-request')
 
 const endpoint = 'https://api.graph.cool/simple/v1/cj54a2vg7uvcm0196cqwzu8nw'
 
+// use varibles to avoid breaking text being added
+// https://github.com/graphcool/graphql-request#using-variables
 const addTweet = (event) => {
   const mutation = `mutation ($tweetText: String!, $userName: String!) {
     createTweet(tweetText: $tweetText, userName: $userName) {
@@ -13,6 +15,7 @@ const addTweet = (event) => {
     tweetText: event.text,
     userName: event.user.screen_name
   }
+
   request(endpoint, mutation, variables)
     .then((data) => console.log(data))
     .catch((err) => console.log('Error: ', err, 'Tweet Text: ', event.text, 'Mutation: ', mutation))
