3a11381365dd7c1307fea2b7e5ee5f0f35578af0
spences10 <spences10apps@gmail.com>
2017-07-20 22:57:35 +0100

    handle retweets

--- src/helpers/dbhandleretweet.js
+++ src/helpers/dbhandleretweet.js
@@ -1,13 +1,21 @@
 const addTweet = require('./dbaddtweet')
 const checkTweet = require('./dbchecktweet')
 
+const track = require('../api/track')
+const config = require('../config')
+
 const handleRetweet = (event) => {
+  if (event.lang != config.twitterConfig.language) return
   // console.log(JSON.stringify(event.lang))
   // console.log(JSON.stringify(event))
   checkTweet(event).then((data) => {
-    console.log(data.length)
+    let count = data.length
+    if (!count >= 0) {
+      addTweet(event)
+      // tweet it with track TODO refactor names!
+      track(event)
+    }
   })
-  addTweet(event)
 }
 
 module.exports = handleRetweet
